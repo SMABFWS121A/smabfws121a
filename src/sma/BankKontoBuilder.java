f0cd1f2ac2c877b722079029866ecf51e1f43464
Thomas Ströder <thomas.stroeder@email.de>
2022-11-02 17:05:04 +0100

    builder pattern

--- src/sma/BankKontoBuilder.java
+++ src/sma/BankKontoBuilder.java
@@ -1,5 +1,21 @@
 package sma;
 
 public class BankKontoBuilder {
+    
+    public int kontoNummer;
+    public String kundenVorname;
+    public int kontoBetrag;
+    public String kundenNachname;
+    public String email;
+    public String adresse;
+    public String telefonNummer;
 
+    public Bankkonto build() {
+        return new Bankkonto(kontoNummer,kundenVorname,kontoBetrag,kundenNachname,
+                                email,adresse,telefonNummer);
+    }
+    
+    
+    
+    
 }
--- src/sma/Bankkonto.java
+++ src/sma/Bankkonto.java
@@ -2,4 +2,33 @@ package sma;
 
 public class Bankkonto {
 
+    public final int kontoNummer;
+    public final String kundenVorname;
+    public final int kontoBetrag;
+    public final String kundenNachname;
+    public final String email;
+    public final String adresse;
+    public final String telefonNummer;
+    
+    Bankkonto(
+        int kontoNummer,
+        String kundenVorname,
+        int kontoBetrag,
+        String kundenNachname,
+        String email,
+        String adresse,
+        String telefonNummer
+    ) {
+        super();
+        this.kontoNummer = kontoNummer;
+        this.kundenVorname = kundenVorname;
+        this.kontoBetrag = kontoBetrag;
+        this.kundenNachname = kundenNachname;
+        this.email = email;
+        this.adresse = adresse;
+        this.telefonNummer = telefonNummer;
+    }
+    
+    
+    
 }
